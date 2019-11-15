29833ae61c2d8f7f91c6edcdfa3f13bd85a4b694
Byron Bustamante <byronbl.12@gmail.com>
2019-11-15 10:44:29 +0000

    Creamos la funcion para eliminar los datos y la imagen de uploads

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -1,4 +1,6 @@
 import {Request, Response} from 'express'
+import path from 'path'
+import fs from 'fs-extra'
 import Photo from '../models/Photo'
 
 export async function getPhotos(req: Request, res: Response): Promise<Response> {
@@ -7,25 +9,35 @@ export async function getPhotos(req: Request, res: Response): Promise<Response> 
 }
 
 export async function getPhoto(req: Request, res: Response): Promise<Response> {
-	const id = req.params.id
-	const photo = await Photo.findById(id)
-	return res.json(photo)
+	const id = req.params.id // Obtenemos el id desde el req
+	const photo = await Photo.findById(id) // Obtenemos los datos con el id
+	return res.json(photo) // Lo convertimos a json y lo devolvemos
 }
 
 export async function createPhoto(req: Request, res: Response): Promise<Response> {
 
-	const { title, description } = req.body
-	const newPhoto = {
+	const { title, description } = req.body // Obtenemos el title y description desde el req.body
+	const newPhoto = { // Creamos un objeto con los datos obtenidos
 		title: title,
 		description: description,
 		imagePath: req.file.path // Obtenemos la ruta de la img a traves del req.file
 	}
-	const photo = new Photo(newPhoto)
-	console.log(photo)
-	await photo.save()
+	const photo = new Photo(newPhoto) // Creamos el modelo con el objeto
+	await photo.save() // Guardamos los datos
+	return res.json({ // Devolvemos los datos guardados en formato json
+		message: 'Photo saved successfully',
+		photo
+	})
+}
+
+export async function deletePhoto(req: Request, res: Response): Promise<Response> {
+	const id = req.params.id
+	const photo = await Photo.findByIdAndRemove(id)
+	if(photo) // Si la foto existe
+		await fs.unlink(path.resolve(photo.imagePath)) // Eliminala pasando la ruta absoluta
 
 	return res.json({
-		message: 'Photo saved successfully',
+		message: 'Photo deleted',
 		photo
 	})
 }
